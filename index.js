d82dc57e5e215535b97b1bd6253e56775eec109e
Mark <me@jsman.ru>
2013-08-21 09:40:28 +0300

    init

---
+++ index.js
@@ -0,0 +1,169 @@
+var request = require('request');
+var async = require('async');
+var gett = exports;
+
+var BASE = 'https://open.ge.tt/1';
+
+exports.auth = function(key, email, password, next) {
+    request({
+        method: 'POST',
+        uri: BASE + '/users/login',
+        json: {
+            apikey: key,
+            email: email,
+            password: password
+        }
+    }, function(err, res, body) {
+        if (err) {
+            next(err);
+            return;
+        }
+
+        if (body.error) {
+            next(body.error);
+            return;
+        }
+
+        next(null, body, body.accesstoken);
+    });
+};
+
+exports.getAllShares = function(token, next) {
+    request({
+        method: 'GET',
+        uri: BASE + '/shares',
+        json: true,
+        qs: {
+            accesstoken: token
+        }
+    }, function(err, res, body) {
+        if (err) {
+            next(err);
+            return;
+        }
+
+        if (body.error) {
+            next(body.error);
+            return;
+        }
+
+        next(null, body);
+    });
+};
+
+exports.createShare = function(token, title, next) {
+    request({
+        method: 'POST',
+        uri: BASE + '/shares/create',
+        qs: {
+            accesstoken: token
+        },
+        json: {
+            title: title
+        }
+    }, function(err, res, body) {
+        if (err) {
+            next(err);
+            return;
+        }
+
+        if (body.error) {
+            next(body.error);
+            return;
+        }
+
+        next(null, body);
+    });
+};
+
+exports.createFile = function(token, shareName, fileName, next) {
+    request({
+        method: 'POST',
+        uri: BASE + '/files/' + shareName + '/create',
+        qs: {
+            accesstoken: token
+        },
+        json: {
+            filename: fileName
+        }
+    }, function(err, res, body) {
+        if (err) {
+            next(err);
+            return;
+        }
+
+        if (body.error) {
+            next(body.error);
+            return;
+        }
+
+        next(null, body);
+    });
+};
+
+exports._uploadFile = function(token, fileMeta, buffer, next) {
+    var upload = fileMeta.upload;
+    var uri = upload.puturl;
+
+    request({
+        method: 'PUT',
+        uri: uri,
+        qs: {
+            accesstoken: token
+        },
+        body: buffer
+    }, function(err, res, body) {
+        if (err) {
+            next(err);
+            return;
+        }
+        next(null, body);
+    });
+};
+
+//--------------------------------------------------------------------
+
+exports.uploadFile = function (key, email, password, fileName, buffer, allDone) {
+    var token;
+    var resultFileMeta;
+
+    async.waterfall([
+
+        function(next) {
+            gett.auth(key, email, password, next);
+        },
+
+        function(body, accesstoken, next) {
+            token = accesstoken;
+            gett.getAllShares(token, next);
+        },
+
+        function(shares, next) {
+
+            if (!shares.length) {
+                gett.createShare(token, 'default', next);
+                return;
+            }
+
+            next(null, shares[0]);
+        },
+
+        function(share, next) {
+            var sharename = share.sharename;
+            gett.createFile(token, sharename, fileName, next);
+        },
+
+        function(fileMeta, next) {
+            resultFileMeta = fileMeta;
+            gett._uploadFile(token, fileMeta, buffer, next);
+        }
+
+
+    ], function(err) {
+        if (err) {
+            allDone(err);
+            return;
+        }
+        allDone(null, resultFileMeta);
+    });
+};
---
+++ test.js
@@ -0,0 +1,14 @@
+var gett = require('./');
+
+var key = '';     //fill
+var email = '';   //fill
+var password = '';//fill
+
+gett.uploadFile(key, email, password,
+    'test.txt', new Buffer('hello world'),
+    function(err, fileMeta) {
+        console.log('err      :', err);
+        console.log('fileMeta :', fileMeta);
+    }
+);
+
